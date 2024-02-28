f1f76089256748ef1dede0b7613ae9e22d19ec03
majk-code <pukalomichal@wp.pl>
2024-02-28 20:31:29 +0100

    upload

---
+++ admin/src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Navbar.css'
+import navlogo from '../../assets/nav-logo.svg'
+import navProfile from '../../assets/nav-profile.svg'
+
+export const Navbar = () => {
+  return (
+    <div className='navbar'>
+        <img src={navlogo} alt="" className="nav-logo" />
+        <img src={navProfile} alt="" className='nav-profile' />
+    </div>
+  )
+}
